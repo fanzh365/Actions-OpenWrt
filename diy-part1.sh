60986916099ffc5abdef882fa6dce6ac9af8b35b
Dawn <466066547@qq.com>
2022-04-08 22:31:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git adguardhome https://github.com/fanzh365/luci-app-adguardhome' >>feeds.conf.default
